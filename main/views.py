0796547e1020ad49a71deb908b012f28c31eefd5
zibanoori <noorizibaa@gmail.com>
2026-06-12 12:10:54 +0330

    render html file in new project

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def index(request):
-    return HttpResponse('<h1>LENZ</h1>')
+    return render(request,"main/index.html")
