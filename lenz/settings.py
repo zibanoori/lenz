a6236164b505eefbd94883bfe895d27b1e951cab
zibanoori <noorizibaa@gmail.com>
2026-06-12 11:00:10 +0330

    Update database name to lenz.sqlite3 and import in gitignore

--- lenz/settings.py
+++ lenz/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'lenz.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'lenz.sqlite3',
     }
 }
 
