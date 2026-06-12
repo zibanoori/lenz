3d00ed999f6c404f9fdd869297ecb36152b08c83
zibanoori <noorizibaa@gmail.com>
2026-06-12 22:32:29 +0330

    add static files to gitignore

--- lenz/settings.py
+++ lenz/settings.py
@@ -78,4 +78,21 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "        static   /"
+STATIC_URL ="static/"
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'main/static',
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL ='media/'
+#
+#
+# # Message Tags
+# from django.contrib.messages import constants as messages
+# MESSAGE_TAGS = {
+#     messages.ERROR:"danger"
+# }
