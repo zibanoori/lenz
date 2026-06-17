f1b365ea70efb7d53159dcd2562e154838708cc6
zibanoori <noorizibaa@gmail.com>
2026-06-17 22:20:49 +0330

    add main details models

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import MyModel
 
+class MoDeleteAdminMixin:
+    def has_delete_permission(self, request, obj=None):
+        return False
 
 @admin.register(MyModel)
-class MyModelAdmin(admin.ModelAdmin):
+class MyModelAdmin(admin.ModelAdmin, MoDeleteAdminMixin):
+    def has_add_permission(self, request):
+        return not MyModel.objects.exists()
+
+
     list_display = ['base_title','website_title','favicon','light_logo','dark_logo','copyright']
     list_display_links = ['favicon','dark_logo','light_logo','copyright']
     list_editable = ['base_title','website_title']
