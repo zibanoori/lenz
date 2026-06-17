0758c18e612ac99ddb6f89bb4064f7284b1b6c43
zibanoori <noorizibaa@gmail.com>
2026-06-17 21:37:43 +0330

    update models and admin in code

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import MyModel
 
-# Register your models here.
+
+@admin.register(MyModel)
+class MyModelAdmin(admin.ModelAdmin):
+    list_display = ['base_title','website_title','favicon','light_logo','dark_logo','copyright']
+    list_display_links = ['favicon','dark_logo','light_logo','copyright']
+    list_editable = ['base_title','website_title']
--- main/models.py
+++ main/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class MyModel(models.Model):
     class Meta:
-        verbose_name ="Main Details"
+        verbose_name = "Main Details"
         verbose_name_plural = "Main Details"
 
     base_title = models.CharField(max_length=50, blank=True)
@@ -10,3 +11,7 @@ class MyModel(models.Model):
     favicon = models.ImageField(upload_to="main", blank=True)
     light_logo = models.ImageField(upload_to="main", blank=True)
     dark_logo = models.ImageField(upload_to="main", blank=True)
+    copyright = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.website_title
