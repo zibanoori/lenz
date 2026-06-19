3ccce8fd59bdbade16de7a1d5e045a6ace276fe8
zibanoori <noorizibaa@gmail.com>
2026-06-19 20:06:45 +0330

    add social media model to index context

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import  MyModel
+from .models import  *
 
 
 def index(request):
     mymodel = MyModel.objects.first()
+    socials = Social.objects.all()
+
 
     context = {
         "mymodel": mymodel,
+        "socials": socials,
     }
     return render(request, "main/index.html", context)
