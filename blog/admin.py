38ef6020a10a8502bc6da30d4c03666eba9778bd
zibanoori <noorizibaa@gmail.com>
2026-06-21 22:31:58 +0330

    add category models in project

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import *
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image', 'title', 'author', 'views', 'comments']
+    list_display_links = ['id']
+    list_editable = ['title', 'author', 'views']
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+    image = models.ImageField(upload_to="blog/category/", blank=True)
+    slug = models.CharField(max_length=50,blank=True)
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    author = models.CharField(max_length=50, blank=True)
+    date = models.CharField(max_length=100, blank=True)
+    views = models.PositiveIntegerField(default=0)
+    comments = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return self.title
