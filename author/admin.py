99b37ddbe7bcb09190469c12ca5190419aad51da
zibanoori <noorizibaa@gmail.com>
2026-06-22 21:50:15 +0330

    update admin models author in code

--- author/admin.py
+++ author/admin.py
@@ -5,7 +5,7 @@ from author.models import Author
 
 @admin.register(Author)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','first_name', 'last_name','email','image')
-    list_display_links = ('id','first_name', 'last_name','email','image')
+    list_display = ('id','first_name', 'last_name','email',)
+    list_display_links = ('id','first_name', 'last_name','email',)
 
 
