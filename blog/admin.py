1d1d6a4a20c16ca9390bec8c679ceb4ba74123d2
zibanoori <noorizibaa@gmail.com>
2026-06-22 20:42:02 +0330

    update models blog

--- blog/admin.py
+++ blog/admin.py
@@ -5,6 +5,6 @@ from blog.models import *
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug')
-    list_display_links = ('title',)
+    list_display = ('id','title', 'slug')
+    list_display_links = ('id','title',)
     list_editable = ('slug',)
