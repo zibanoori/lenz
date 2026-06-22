ae89704b92a2c290f14729b5646bc9b59013543d
zibanoori <noorizibaa@gmail.com>
2026-06-22 21:12:21 +0330

    add new model image for author

--- author/models.py
+++ author/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=100,blank=True)
     last_name = models.CharField(max_length=100,blank=True)
+    image = models.ImageField(upload_to='images/',blank=True)
     biography = models.TextField(blank=True)
     facebook = models.URLField(blank=True)
     twitter = models.URLField(blank=True)
