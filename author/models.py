8cac575a160d20beb725d22b05a005cec2f092b7
zibanoori <noorizibaa@gmail.com>
2026-06-22 20:22:02 +0330

    add urls field to code

--- author/models.py
+++ author/models.py
@@ -3,4 +3,10 @@ from django.db import models
 class Author(models.Model):
     first_name = models.CharField(max_length=100,blank=True)
     last_name = models.CharField(max_length=100,blank=True)
+    biography = models.TextField(blank=True)
+    facebook = models.URLField(blank=True)
+    twitter = models.URLField(blank=True)
+    pinterest = models.URLField(blank=True)
+    linkedin = models.URLField(blank=True)
+    email = models.EmailField(blank=True)
     
