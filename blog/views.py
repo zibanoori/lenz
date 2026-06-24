ff033cc8f97972869e4f3214386e7078260dc609
zibanoori <noorizibaa@gmail.com>
2026-06-24 21:37:56 +0330

    modieied urls and views to blog for slug

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import *
+
+
+def post_detail(request, post_slug):
+    post = get_object_or_404(Post,slug=post_slug)
+    context = {"post":post}
