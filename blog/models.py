29461852ff6ddef25c951bbdc5d925119867ef97
zibanoori <noorizibaa@gmail.com>
2026-06-24 19:36:12 +0330

    debug models app blog

--- blog/models.py
+++ blog/models.py
@@ -24,18 +24,18 @@ class Post(models.Model):
         verbose_name_plural = "Posts"
 
     category = models.ForeignKey(Category,on_delete=models.CASCADE)
-    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+    author = models.ForeignKey(Author,on_delete=models.DO_NOTHING)
     title = models.CharField(max_length=200,blank=True)
     slug = models.SlugField(max_length=250,blank=True, unique=True)
     image = models.ImageField(upload_to='blog/%Y/%m',blank=True)
-    send_date = models.DateField(default=datetime.date.today,blank=True)
+    send_date = models.DateTimeField(default=datetime.now, blank=True)
     views = models.PositiveIntegerField(default=0,blank=True)
     comments = models.PositiveIntegerField(default=0,blank=True)
     full_text = models.TextField(blank=True)
     abstract = models.TextField(blank=True)
     tags = models.CharField(max_length=200,blank=True)
     is_published = models.BooleanField(default=False)
-    published_date = models.DateField(default=datetime.date.today, blank=True)
+    published_date = models.DateField(default=datetime.now, blank=True)
 
     def save(self, *args, **kwargs):
         if not self.slug:
