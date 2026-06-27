a5a28abcd6e3a960f8e769a4712e67d77b40e193
zibanoori <noorizibaa@gmail.com>
2026-06-27 16:01:43 +0330

    debug new app post-details in project

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .views import *
 
-urlpatterns = [path("post/<slug:post_slug>/",post_delete, name="post_detail")]
+urlpatterns = [path("post/<slug:post_slug>/",post_detail, name="post_detail")]
--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,8 @@ from .models import *
 
 def post_detail(request, post_slug):
     post = get_object_or_404(Post,slug=post_slug)
-    context = {"post":post}
+    context = {
+        "post":post
+    }
+
+    return render(request, "blog/post_details.html", context=context)
