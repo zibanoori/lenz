ebeb6fd9e07736e4cc79d9e4f64cddfbd11996e7
zibanoori <noorizibaa@gmail.com>
2026-06-30 19:01:04 +0330

    add new app in urls main template

--- lenz/urls.py
+++ lenz/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("blog/", include("blog.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
 
 
