f595d8976124eb4fb966f96b355f521cc4bc2531
zibanoori <noorizibaa@gmail.com>
2026-07-04 12:32:05 +0330

    import and request form

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("login-register", logreg, name="logreg")
-
+    path("login-register", logreg, name="logreg"),
+    path("signup", signup, name="signup"),
+    path("signin", logreg, name="signin"),
+    path("signout", logreg, name="signout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib import messages
+
 
 def logreg(request):
-    return render(request,"accounts/logreg.html")
+    return render(request, "accounts/logreg.html")
 
+
+def register(request):
+
+
+    if request.method == "POST":
+        username = request.POST.get["username"]
+        password = request.POST.get["password"]
