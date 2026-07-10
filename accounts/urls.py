7e03fdce41f567123b621e566b21d3f8b987bb8d
zibanoori <noorizibaa@gmail.com>
2026-07-10 10:42:44 +0330

    editing and correcting form appearance problems

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
-import logreg
+
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path("login-register", logreg, name="logreg"),
-    path("signup", signup, name="signup"),
+    path("signup",register, name="signup"),
     path("signin", logreg, name="signin"),
+
     path("signout", logreg, name="signout"),
 ]
